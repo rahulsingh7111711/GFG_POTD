9901f2eaa830206485c7a9165b0c4c05a6da2656
Rahul Singh <87928472+rahulsingh7111711@users.noreply.github.com>
2023-08-05 01:41:17 +0530

    Create Solution.cpp

---
+++ 4 Aug'23 Reverse a Stack/Solution.cpp
@@ -0,0 +1,26 @@
+//User function Template for C++
+class Solution{
+public:
+    void insertAtLast(stack<int> &St,int val){
+        if(St.size()==0){
+            St.push(val);
+            return;
+        }
+        
+            int el=St.top();
+            St.pop();
+            insertAtLast(St,val);
+            St.push(el);
+    }
+    
+    void Reverse(stack<int> &St){
+        if(St.size()==0) return;
+        
+        int el=St.top();
+        St.pop();
+        
+        Reverse(St);
+        
+        insertAtLast(St,el);
+    }
+};
